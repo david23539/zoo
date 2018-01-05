50498e2d66582390efde0b4c01e31fcd87052047
david23539 <david23539@gmail.com>
2018-01-05 18:46:13 +0100

    fix en las imagenes

--- controller/user.js
+++ controller/user.js
@@ -147,6 +147,7 @@ function uploadImage(req, res) {
       var file_path = req.files.image.path;
       var file_split = file_path.split('/');
       var filename = file_split[3];
+      console.dir(filename);
       var ext_split = filename.split('/.');
       var file_ext = ext_split[1];
       if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
